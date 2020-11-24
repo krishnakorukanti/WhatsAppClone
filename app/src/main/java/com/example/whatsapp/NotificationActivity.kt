40e2bfdee832ec0e9a8cca21edde20e33dd2a2c9
Shivaraj <janeshiv44@gmail.com>
2020-11-24 22:25:35 +0530

    NotificationActivity layout creation

--- app/src/main/java/com/example/whatsapp/NotificationActivity.kt
+++ app/src/main/java/com/example/whatsapp/NotificationActivity.kt
@@ -7,6 +7,5 @@ class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
-
     }
 }
