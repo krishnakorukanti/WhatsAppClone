679c50220622099be3974c7f2d987c7337411d00
Shivaraj <janeshiv44@gmail.com>
2020-11-25 00:17:05 +0530

    AccountActivity layout done!

--- app/src/main/java/com/example/whatsapp/AccountActivity.kt
+++ app/src/main/java/com/example/whatsapp/AccountActivity.kt
@@ -3,10 +3,9 @@ package com.example.whatsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class NotificationActivity : AppCompatActivity() {
+class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
-
+        setContentView(R.layout.activity_account)
     }
 }
