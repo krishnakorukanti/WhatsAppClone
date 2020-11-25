2e4a98c4fed3ec9c62bb664b1bcdb34132979540
gouravguru <gourav97031@gmail.com>
2020-11-25 17:09:15 +0530

    call_fragment

---
+++ app/src/main/java/com/example/whatsapp/ViewPagerAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.whatsapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.whatsapp.fragments.CallsFragment
+import com.example.whatsapp.fragments.ChatsFragment
+import com.example.whatsapp.fragments.StatusFragment
+import java.util.*
+
+class ViewPagerAdapter(
+    fragmentManager: FragmentManager, private val fragmentList: List<Fragment?>? = ArrayList(),
+    private val fragmentTitles: List<String?>? = ArrayList()
+
+) :FragmentStatePagerAdapter(fragmentManager) {
+
+
+
+    override fun getCount(): Int {
+      return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+   when(position){
+       0 -> {
+           return ChatsFragment()
+       }
+       1 -> {
+           return StatusFragment()
+       }
+       2 -> {
+          return CallsFragment()
+       }else ->{
+       return ChatsFragment()
+   }
+   }
+
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        when(position){
+            0 ->{
+                return "Chats"
+            }
+            1->{
+                return "status"
+            }
+            2->{
+                return "calls"
+            }
+        }
+        return super.getPageTitle(position)
+    }
+
+}
--- app/src/main/java/com/example/whatsapp/activities/HomeActivity.kt
+++ app/src/main/java/com/example/whatsapp/activities/HomeActivity.kt
@@ -1,31 +1,38 @@
 package com.example.whatsapp.activities
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
 import com.example.whatsapp.R
+import com.example.whatsapp.ViewPagerAdapter
+import com.example.whatsapp.adapter.CallsAdapter
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        view_pager.adapter = ViewPagerAdapter(supportFragmentManager)
 
-        setViewPagerAdapter()
+        tabs.setupWithViewPager(view_pager)
+
+
+
+       /* setViewPagerAdapter()
         val toolbar = findViewById(R.id.toolbar) as Toolbar?
         setSupportActionBar(toolbar)
         toolbar?.title = "WhatsApp"
-
+*/
     }
 
-    private fun setViewPagerAdapter() {
+   /* private fun setViewPagerAdapter() {
 
-    }
+    }*/
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+   /* override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuInflater = menuInflater
         menuInflater.inflate(R.menu.menu_settings, menu)
         return true
-    }
+    }*/
 }
