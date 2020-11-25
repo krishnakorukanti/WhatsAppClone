163a5a785dcba5f2a048c72bb3496a54f2fa4037
sai krishna <crishnakorukanti@gmail.com>
2020-11-25 15:21:41 +0530

    # User Data Successfully Uploaded the data to FireBase RealTime DataBase.

--- app/src/main/java/com/example/whatsapp/activities/UserDataActivity.kt
+++ app/src/main/java/com/example/whatsapp/activities/UserDataActivity.kt
@@ -1,6 +1,5 @@
 package com.example.whatsapp.activities
 
-<<<<<<< HEAD
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,12 +7,10 @@ import android.widget.Toast
 import com.example.whatsapp.R
 import com.example.whatsapp.data.Users
 import com.google.firebase.auth.FirebaseAuth
-
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
-
 import kotlinx.android.synthetic.main.activity_user_data.*
 
 class UserDataActivity : AppCompatActivity() {
@@ -25,16 +22,15 @@ class UserDataActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_data)
         auth = Firebase.auth
         nextHomeBtn.setOnClickListener {
-            if (profileAbout.text.isEmpty() || profileNameUser.text.isEmpty()) {
-                Toast.makeText(this@UserDataActivity, "Invalid Entry", Toast.LENGTH_LONG).show()
+            if (profileAbout.text.isEmpty()||profileNameUser.text.isEmpty()){
+                Toast.makeText(this@UserDataActivity,"Invalid Entry",Toast.LENGTH_LONG).show()
 
-            } else {
+            }else{
                 updateFirebaseDatabase()
             }
         }
 
     }
-
     private fun updateFirebaseDatabase() {
         val userId = auth.currentUser?.uid!!
         databaseReference =
@@ -43,29 +39,11 @@ class UserDataActivity : AppCompatActivity() {
             profileNameUser.text.toString(),
             this.auth.currentUser?.phoneNumber.toString(),
             profileAbout.text.toString(),
-            auth.uid
-        )
+            auth.uid)
         //Adds the user to the database
         databaseReference.setValue(user).addOnCompleteListener {
-            val intent = Intent(this@UserDataActivity, HomeActivity::class.java)
+            val intent = Intent(this@UserDataActivity,HomeActivity::class.java)
             startActivity(intent)
         }
     }
 }
-=======
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.whatsapp.R
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-
-class UserDataActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var databaseReference: DatabaseReference
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_data)
-    }
-}
->>>>>>> b9f9828... # Authentication using Otp.
