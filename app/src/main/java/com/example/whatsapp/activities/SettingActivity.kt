01972705b82e266efe95655c14d656d499307d5d
Shivaraj <janeshiv44@gmail.com>
2020-11-26 14:14:14 +0530

    linking of all the parts are done!!

--- app/src/main/java/com/example/whatsapp/activities/SettingActivity.kt
+++ app/src/main/java/com/example/whatsapp/activities/SettingActivity.kt
@@ -3,14 +3,28 @@ package com.example.whatsapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import com.example.whatsapp.AccountActivity
+import com.example.whatsapp.NotificationActivity
 import com.example.whatsapp.R
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        account_layout.setOnClickListener(View.OnClickListener {
+            val intent1=Intent(this,AccountActivity::class.java)
+            startActivity(intent1)
+        })
+
+        notification_layout.setOnClickListener(View.OnClickListener {
+            val intent1=Intent(this,NotificationActivity::class.java)
+            startActivity(intent1)
+        })
 
         val llInviteFriend = findViewById<LinearLayout>(R.id.llInviteFriend)
         llInviteFriend.setOnClickListener {
@@ -41,4 +55,5 @@ class SettingActivity : AppCompatActivity() {
             startActivity(homeIntent)
         }
     }
+
 }
